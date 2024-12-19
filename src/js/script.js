6de96ab30533b77a32233412c076b719c1e8de98
Nana <evylaveras@gmail.com>
2024-12-18 22:31:51 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("verifyButton").addEventListener("click", function () {
+  const num1 = parseInt(document.getElementById("num1").textContent, 10);
+  const num2 = parseInt(document.getElementById("num2").textContent, 10);
+  const correctResult = num1 + num2;
+
+  const userResult = parseInt(document.getElementById("userResult").value, 10);
+  const resultado = document.getElementById("resultado");
+
+  if (userResult === correctResult) {
+    resultado.textContent = "Resposta correta!";
+    resultado.style.color = "green";
+  } else {
+    resultado.textContent = "Resposta incorreta.";
+    resultado.style.color = "red";
+  }
+});
